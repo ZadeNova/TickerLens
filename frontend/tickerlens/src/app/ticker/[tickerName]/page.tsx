da6f21be673fca94a85b2d23351d07061f53f167
Zade <muhderfannnn@gmail.com>
2025-05-10 23:04:31 +0800

    Added Accordion component. added slight modification of UI to Tickerpage. Comitting so that I can do this project on my laptop.

--- frontend/tickerlens/src/app/ticker/[tickerName]/page.tsx
+++ frontend/tickerlens/src/app/ticker/[tickerName]/page.tsx
@@ -1,13 +1,22 @@
-// Configure metadata for this page
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
 
-export async function generateMetadata({ params } : {params: { tickerName: string}}){
+// Configure metadata for this page
+export async function generateMetadata({
+	params,
+}: {
+	params: { tickerName: string };
+}) {
 	const { tickerName } = await params;
 	const tickerName_decoded = decodeURIComponent(tickerName);
 	return {
 		title: `${tickerName_decoded}`,
-		description: ""
-	}
-
+		description: "",
+	};
 }
 
 // export const metadata: Metadata = {
@@ -17,20 +26,26 @@ export async function generateMetadata({ params } : {params: { tickerName: strin
 
 //export async function generateMetadata()
 
-async function getTickerData(ticker: string){
-	
-	const response = await fetch(`http://127.0.0.1:8000/api/v1/stocks/${ticker}`, { 
-		method: 'POST',
-		next: { revalidate: 900 }, });
+async function getTickerData(ticker: string) {
+	const response = await fetch(
+		`http://127.0.0.1:8000/api/v1/stocks/${ticker}`,
+		{
+			method: "POST",
+			next: { revalidate: 900 },
+		}
+	);
 
 	if (!response.ok) return null;
 
 	const data = await response.json();
 	//console.log(data);
 	return data;
-};
+}
 
-export default async function tickerInfo({ params, }: { params: { tickerName: string }; 
+export default async function tickerInfo({
+	params,
+}: {
+	params: { tickerName: string };
 }) {
 	const { tickerName } = await params;
 	console.log(tickerName);
@@ -39,22 +54,26 @@ export default async function tickerInfo({ params, }: { params: { tickerName: st
 		symbol: "RKLB",
 		displayName: "RocketLab USA",
 		industry: "Aerospace",
-	}
+	};
 	return (
 		<>
 			<div className="p-6 bg-white dark:bg-gray-900 min-h-screen">
 				<div className="max-w-5xl mx-auto">
-					<h1 className="text-2xl font-bold mb-4">{data.displayName} ({data.symbol})</h1>
+					<h1 className="text-2xl font-bold mb-4">
+						{data.displayName} ({data.symbol})
+					</h1>
 					<div className="flex">
 						<h3 className="font-bold mb-4">Current Price: ${}</h3>
 					</div>
-					<div className="rounded-2xl bg-gray-200 p-2 w-full h-24">
+					<div className="rounded-lg shadow-md bg-gray-200 p-2 w-full h-24">
 						<div className="justify-center items-center flex">
-							<span className="text-indigo-500 font-bold">Chart is still in development.</span>
+							<span className="text-indigo-500 font-bold">
+								Chart is still in development.
+							</span>
 						</div>
 					</div>
 					{/* Display stock basic data first */}
-					<div className="bg-gray-100 rounded-2xl p-2 w-full mt-2 h-24 sm:h-32 sm:mt-4 ">
+					<div className="bg-indigo-50 rounded-lg shadow-md shadow-indigo-100 p-2 w-full mt-2 h-24 sm:h-32 sm:mt-4 ">
 						<dl className="grid grid-cols-4 gap-x-4 gap-y-2 md:grid-cols-8 text-sm">
 							<dt>Previous Close</dt>
 							<dd></dd>
@@ -75,10 +94,15 @@ export default async function tickerInfo({ params, }: { params: { tickerName: st
 						</dl>
 					</div>
 
-
 					{/* Display the company's basic background */}
-
-					
+					<Accordion type="single" collapsible>
+						<AccordionItem value="item-1">
+							<AccordionTrigger>Company&apos;s Summary</AccordionTrigger>
+							<AccordionContent>
+								Yes. It adheres to the WAI-ARIA design pattern.
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
 				</div>
 			</div>
 		</>
